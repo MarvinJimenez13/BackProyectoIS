c4cbbd0a70dd06f0cb48d261c56c8c0808235afd
MarvinJimenez13 <marvinjiga@gmail.com>
2020-06-07 00:02:18 -0500

    Pruebas recibiendo datos desde Angular.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
- const express = require('express');
- const  app = express();
- app.set('port', 3000);
+const express = require('express');
+const  app = express();
+app.set('port', 3000);
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+    app.options('*', (req, res) => {
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.send();
+    });
+});
 
- app.listen(app.get('port'), () => {
-     console.log('Server on port:', app.get('port'));
+app.get('/', (req, res) =>{
+    res.send("<h1>Servidor funcionando.</h1>")
+});
+
+app.post('/resistencia', (req, res) =>{
+    console.log(req.body);
+    res.send();
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Server on port:', app.get('port'));
  });
