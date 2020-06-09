0a7d2e5a7f5aa4809b72786bd07049da15b71489
MarvinJimenez13 <marvinjiga@gmail.com>
2020-06-09 09:39:30 -0500

    Métodos, get, put, delete, post funcionando.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,44 @@
 const express = require('express');
 const app = express();
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
 var Resistencia = require('./../models/Resistencia.js');
 var path = require("path");
 var publicPath=path.resolve(__dirname,"public");
 
+mongoose.set('useFindAndModify', false);
 app.use(express.static(publicPath));
 app.set('port', 3000);
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://pruebaapipro.000webhostapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-    app.options('https://pruebaapipro.000webhostapp.com', (req, res) => {
+    app.options('*', (req, res) => {
         res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
         res.send();
     });
 });
 
-moongose.connect('mongodb+srv://Marvin:is@cluster0-6ms30.mongodb.net/ProyectoIS?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://Marvin:is@cluster0-6ms30.mongodb.net/ProyectoIS?retryWrites=true&w=majority',
         { useNewUrlParser: true })
         .then(() => {
             console.log('Conectado a Mongo DB Atlas');
         })
         .catch(err => console.log(err));
 
+/*Ruta de presentación*/        
 app.get('/', (req, res) =>{
     res.sendFile(path.join( __dirname + '/public/index.html'));
 });
 
+/*Obtener detalles de una resistencia*/
 app.get('/resistencia/:id', (req, res) =>{
-    Resistencia.find({_id:req.params.id},function(err,documentos){
-        res.send("<h1>"+documentos+"</h1>");
-        console.log(documentos);
+    Resistencia.find({_id:req.params.id},function(err, documentos){
+        res.send("<h1>" + documentos + "</h1>");
       });
 });
 
+/*Agregar una resistencia*/
 app.post('/resistencia', (req, res) =>{
     console.log(req.body);
     var resistencia = Resistencia({
@@ -44,7 +47,6 @@ app.post('/resistencia', (req, res) =>{
         tolerancia: req.body.tolerancia,
         potencia: req.body.potencia
     });
-
     resistencia.save(function(err, data) {
         if (err)
             res.send('Error al registrar resistencia.');
@@ -53,37 +55,27 @@ app.post('/resistencia', (req, res) =>{
     });
 });
 
+/*Eliminar una resistencia*/
 app.delete('/resistencia/:id', (req, res)=>{
-  Resistencia.findOneAndDelete({_id:req.params.id}, function(err,data){
+  Resistencia.findOneAndDelete({_id:req.params.id}, function(err, data){
     if(err)
-      res.status(404).send('No se pudo eliminar la resistencia')
+        res.status(404).send('No se pudo eliminar la resistencia.')
       else
-     res.status(200).send('Resistencia eliminada');
-
+        res.status(200).send('Resistencia eliminada correctamente.');
   });
 });
 
-app.put('resistencia/:id',(req,res)=>{
-  Resistencia.findOneAndUpdate({_id:req.params.id}, function(err,data){
-
-    console.log(req.body);
-    var resistencia = Resistencia({
-        tipo: req.body.tipo,
-        valorNominal: req.body.nominal,
-        tolerancia: req.body.tolerancia,
-        potencia: req.body.potencia
+/*Actualizar una resistencia*/
+app.put('/resistencia',(req, res)=>{
+    Resistencia.findOneAndUpdate({_id:req.body.id}, {tipo:req.body.tipo}, {valorNominal:req.body.nominal},
+        {tolerancia:req.body.tolerancia}, {potencia:req.body.potencia}, function(err, data){
+        if(err)  
+            res.send("Error al actualizar la resistencia.");
+        else    
+            res.send("Resistencia actualizada correctamente.");
     });
-
-    resistencia.save(function(err, data) {
-        if (err)
-            res.send('No se pudo guardar los cambios');
-        else
-            res.send('Cambios guardados');
-  });
-
 });
 
-});
 app.listen(process.env.PORT || app.get('port'), () => {
     console.log('Server on port:', app.get('port'));
  });
