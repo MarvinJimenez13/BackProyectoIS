a48222d8f787213758763410581062baa7062145
MarvinJimenez13 <marvinjiga@gmail.com>
2020-06-15 01:55:59 -0500

    Quitando PUT cors.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
     app.options('https://pruebaapipro.000webhostapp.com', (req, res) => {
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE, OPTIONS');
         res.send();
     });
 });
